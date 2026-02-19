014930e2880076158bf3ca027a6f152fce921374
erges699 <erges699@gmail.com>
2026-02-19 11:29:23 +0300

    w1-erges699-37-2-7-jwt-config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { appConfig } from './config/app.config';
+import { jwtConfig } from './config/jwt.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [appConfig],
+      load: [appConfig, jwtConfig],
       isGlobal: true,
     }),
     UsersModule,
