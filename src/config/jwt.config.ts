63cf0d6602ad158246fc57b065bac7c2120bf1af
Serge Balyaba <erges699@gmail.com>
2026-02-19 15:39:19 +0300

    Update jwt.config.ts

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -3,4 +3,6 @@ import { registerAs } from '@nestjs/config';
 export const jwtConfig = registerAs('jwt', () => ({
   secret: process.env.JWT_SECRET || 'defaultSecretKey',
   expiresIn: process.env.JWT_EXPIRES_IN || '1h',
+  refreshToken: process.env.JWT_REFRESH_SECRET || 'refreshSecretKey',
+  refreshExpiresIn: (process.env.JWT_REFRESH_EXPIRES_IN || '7d'),
 }));
