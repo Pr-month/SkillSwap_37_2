679dd8e3f89059c275afa6b0e94555a5c03e78a3
Serge Balyaba <erges699@gmail.com>
2026-02-19 15:15:20 +0300

    Update app.config.ts

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export const appConfig = registerAs('APP_CONFIG', () => ({
-  port: parseInt(process.env.PORT ?? '3000', 3000),
+  port: parseInt(process.env.PORT ?? '3000', 10),
   hashSalt: parseInt(process.env.HASH_SALT ?? '10', 10),
   environment: process.env.NODE_ENV || 'development',
   cors: {
@@ -9,3 +9,5 @@ export const appConfig = registerAs('APP_CONFIG', () => ({
     credentials: true,
   },
 }));
+
+export type IConfig = ConfigType<typeof appConfig>;
