d0f6d9b31a4f61e9ea57d81d676e1aa2c8cbcdd1
lavdl <maxssw8@gmail.com>
2026-02-19 10:04:21 +0400

    feature/user-entity

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,50 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export enum UserRole {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+}
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  about: string;
+
+  @Column({ type: 'date' })
+  birthdate: Date;
+
+  @Column()
+  city: string;
+
+  @Column()
+  gender: string;
+
+  @Column()
+  avatar: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role: UserRole;
+
+  @Column()
+  refreshToken: string;
+}
