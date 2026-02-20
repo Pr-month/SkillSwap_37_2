d60701a565d3cfea483e8c5a2c81acd1683a769d
Shelepov Sergey <shelepov-sa@yandex.ru>
2026-02-20 08:39:14 +0300

    реализовал регистрацию пользователя

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,16 @@
-export class CreateAuthDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateAuthDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
