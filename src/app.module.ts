2204b54e629e4884838d0a496a0c1b6ff42ac8da
Roland <machodin@yandex.ru>
2026-02-21 16:43:34 +0300

    fix(pr) fixing imports and packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { appConfig } from './config/app.config';
 import { jwtConfig } from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
