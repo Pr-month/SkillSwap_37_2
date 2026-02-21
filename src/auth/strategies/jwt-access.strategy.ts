37f34fd951e433eb625ed794e36aac18c9ad7663
Shelepov Sergey <shelepov-sa@yandex.ru>
2026-02-21 08:33:50 +0300

    переиспользовал тип JwtPayload

--- src/auth/strategies/jwt-access.strategy.ts
+++ src/auth/strategies/jwt-access.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from '../auth.types';
 
 @Injectable()
 export class JwtAccessStrategy extends PassportStrategy(Strategy, 'jwt-access') {
@@ -12,7 +13,7 @@ export class JwtAccessStrategy extends PassportStrategy(Strategy, 'jwt-access') 
     });
   }
 
-  validate(payload: { sub: number; email: string; role: string }) {
+  validate(payload: JwtPayload) {
     return {
       id: payload.sub,
       email: payload.email,
