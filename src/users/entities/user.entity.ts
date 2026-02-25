f2c6c01c7c7d2f706d4ad9933d560f2ef36fddc9
Roland <machodin@yandex.ru>
2026-02-25 17:23:19 +0300

    restore user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,46 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Gender, UserRole } from '../users.enums';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  about: string;
+
+  @Column({ type: 'date', nullable: true })
+  birthdate: Date;
+
+  @Column({ nullable: true })
+  city: string;
+
+  @Column({
+    type: 'enum',
+    enum: Gender,
+    nullable: true,
+  })
+  gender: Gender;
+
+  @Column({ nullable: true })
+  avatar: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role: UserRole;
+
+  @Column({ type: 'varchar', nullable: true })
+  refreshToken: string | null;
+}
