cb10c5868a789af8fb0edcfdda37c8363f9a0131
Roland <machodin@yandex.ru>
2026-02-25 21:45:22 +0300

    resolve conflicts

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,5 +1,5 @@
-import { registerAs } from '@nestjs/config';
-import { DataSourceOptions } from 'typeorm';
+import { ConfigType, registerAs } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dbConfig = registerAs(
   'DB_CONFIG',
@@ -8,10 +8,14 @@ export const dbConfig = registerAs(
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 5432,
     username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'password',
+    password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'skillswap',
     synchronize: process.env.NODE_ENV !== 'production',
     logging: false,
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   }),
 );
+
+export type IDbConfig = ConfigType<typeof dbConfig>;
+
+export const AppDataSource = new DataSource(dbConfig());
