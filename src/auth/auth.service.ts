c3bca939db4cea15b5ac7bcf08c3e7d52e0fd54e
erges699 <erges699@gmail.com>
2026-03-01 14:24:57 +0300

    w2-erges699-45

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import {
   ConflictException,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -8,12 +9,17 @@ import { UsersService } from '../users/users.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import * as bcrypt from 'bcrypt';
+import { ConfigType } from '@nestjs/config';
+import { jwtConfig } from '../config/jwt.config';
+import { StringValue } from 'ms';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly config: ConfigType<typeof jwtConfig>,
   ) {}
 
   private async getTokens(user: { id: number; email: string }) {
@@ -25,7 +31,7 @@ export class AuthService {
 
     const accessToken = await this.jwtService.signAsync(payload);
     const refreshToken = await this.jwtService.signAsync(payload, {
-      expiresIn: Number(process.env.JWT_REFRESH_TOKEN_EXPIRES_IN) || 604800,
+      expiresIn: this.config.refreshExpiresIn as StringValue,
     });
 
     return {
@@ -43,7 +49,7 @@ export class AuthService {
   }
 
   async register(createAuthDto: CreateAuthDto) {
-    const existingUser = this.usersService.findByEmail(createAuthDto.email);
+    const existingUser = await this.usersService.findByEmail(createAuthDto.email);
 
     if (existingUser) {
       throw new ConflictException('Пользователь с таким email уже существует');
@@ -51,7 +57,7 @@ export class AuthService {
 
     const hashedPassword = await bcrypt.hash(createAuthDto.password, 10);
 
-    const user = this.usersService.createFromAuth({
+    const user = await this.usersService.createFromAuth({
       ...createAuthDto,
       password: hashedPassword,
     });
@@ -66,7 +72,7 @@ export class AuthService {
   }
 
   async login(loginAuthDto: LoginAuthDto) {
-    const user = this.usersService.findByEmail(loginAuthDto.email);
+    const user = await this.usersService.findByEmail(loginAuthDto.email);
 
     if (!user) {
       throw new UnauthorizedException('Неверный email или пароль');
