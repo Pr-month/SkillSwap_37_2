76349394ac0d8e608658a9c87d0012b31769bb41
Baziba <lisanovd@yandex.ru>
2026-03-02 20:14:52 +0300

    feat: category.entity

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,1 +1,19 @@
-export class Category {}
+import {Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity('categories')
+export class Category {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @ManyToOne(() => Category, (category) => category.children, {
+        nullable: true,
+        onDelete: 'SET NULL',
+    })
+    parent: Category | null;
+
+    @OneToMany(() => Category, (category) => category.parent)
+    children: Category[];
+}
