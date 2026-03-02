a9d9cd523645902138bbe424781918926067ad9d
Alexey Zimin <aazimin101@gmail.com>
2026-03-02 21:33:42 +0300

    update/create-auth.dto

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsDate, IsEmail, IsNotEmpty, IsString, IsUrl, MaxDate, MinDate, MinLength } from 'class-validator';
 
 export class CreateAuthDto {
   @IsString()
@@ -13,4 +13,42 @@ export class CreateAuthDto {
   @IsNotEmpty()
   @MinLength(6)
   password: string;
+
+  @IsString()
+  //uncomment if required
+  // @IsNotEmpty()
+  about: string;
+
+  @IsDate()
+  //uncomment if required
+  // @IsNotEmpty()
+  @MinDate(new Date("1900-01-01"))
+  @MaxDate(new Date())
+  birthdate : string;
+
+  @IsString()
+  //uncomment if required
+  // @IsNotEmpty()
+  @MinLength(2)
+  city: string;
+
+  @IsString()
+  //uncomment if required
+  // @IsNotEmpty()
+  gender: string;
+
+  @IsUrl()
+  //uncomment if required
+  // @IsNotEmpty()
+  avatar: string;
+
+  @IsString()
+  //uncomment if required
+  // @IsNotEmpty()
+  skillCategoryName: string;
+
+  @IsString()
+  //uncomment if required
+  // @IsNotEmpty()
+  skillSubCategoryName: string;
 }
