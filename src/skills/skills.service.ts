faac6f546774f9b7e958850b1fb680d973b17212
Roland <machodin@yandex.ru>
2026-03-02 18:12:11 +0300

    resolve conflicts

--- src/skills/skills.service.ts
+++ src/skills/skills.service.ts
@@ -3,7 +3,6 @@ import {
   NotFoundException,
   ForbiddenException,
 } from '@nestjs/common';
-import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateSkillDto } from './dto/create-skill.dto';
