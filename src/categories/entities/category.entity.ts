6f0c29a129a6e084d928ef0bebc6f15da44c9ebd
erges699 <erges699@gmail.com>
2026-03-05 10:09:21 +0300

    w3-erges699-109-seed-skills-20260305

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,19 +1,30 @@
-import {Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Skill } from '../../skills/entities/skill.entity';
 
 @Entity('categories')
 export class Category {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @ManyToOne(() => Category, (category) => category.children, {
-        nullable: true,
-        onDelete: 'SET NULL',
-    })
-    parent: Category | null;
+  @ManyToOne(() => Category, (category) => category.children, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  parent: Category | null;
 
-    @OneToMany(() => Category, (category) => category.parent)
-    children: Category[];
+  @OneToMany(() => Category, (category) => category.parent)
+  children: Category[];
+
+  @OneToMany(() => Skill, (skill) => skill.category)
+  skills: Skill[];
 }
