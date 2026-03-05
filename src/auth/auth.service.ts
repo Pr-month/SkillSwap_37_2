cca33a6db8ba95a9e3a6b528212c6b1aa415cc21
lavdl <maxssw8@gmail.com>
2026-03-05 21:52:07 +0400

    fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -28,6 +28,9 @@ export class AuthService {
       expiresIn: Number(process.env.JWT_REFRESH_TOKEN_EXPIRES_IN) || 604800,
     });
 
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+    await this.usersService.updateRefreshToken(user.id, hashedRefreshToken);
+
     return {
       accessToken,
       refreshToken,
@@ -61,8 +64,6 @@ export class AuthService {
     const userData = this.toPublicUser(user);
     const tokens = await this.getTokens(userData);
 
-    await this.usersService.updateRefreshToken(user.id, tokens.refreshToken);
-
     return {
       user: userData,
       tokens,
@@ -87,8 +88,6 @@ export class AuthService {
 
     const userData = this.toPublicUser(user);
     const tokens = await this.getTokens(userData);
-
-    await this.usersService.updateRefreshToken(user.id, tokens.refreshToken);
 
     return {
       user: userData,
