621f6b343898d31af45474b2b8b33c491d64efaa
Baziba <lisanovd@yandex.ru>
2026-03-11 20:19:22 +0300

    fix: добавил посев категорий с сеялку

--- src/scripts/seeder.ts
+++ src/scripts/seeder.ts
@@ -1,6 +1,7 @@
 import { seedAdmin } from './seed-admin';
 import { seedUsers } from './seed-users';
 import { seedSkills } from './seed-skills';
+import { seedCategories } from './seed-categories';
 
 async function seeder() {
   console.log('Запуск сидинга администратора...');
@@ -14,6 +15,10 @@ async function seeder() {
   console.log('Запуск сидинга навыков...');
   await seedSkills();
   console.log('Сидинг навыков завершён.');
+
+  console.log('Запуск сидинга категорий');
+  await seedCategories();
+  console.log('Сидинг категорий завершён');
 
   console.log('Все сидинги успешно выполнены.');
 }
